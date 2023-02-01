60286dbebc5986dd0217eea4f9e6216cacb3293e
Paul Olofsson <paul.olofsson@visma.com>
2023-02-01 13:35:52 +0100

    test

--- src/Cdkproj/CdkprojStack.cs
+++ src/Cdkproj/CdkprojStack.cs
@@ -1,5 +1,4 @@
 using Amazon.CDK;
-using Amazon.CDK.AWS.S3;
 using Amazon.CDK.Pipelines;
 using Constructs;
 
@@ -9,13 +8,17 @@ namespace Cdkproj
     {
         internal CdkprojStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-
             var pipeline = new CodePipeline(this, "pipeline", new CodePipelineProps
             {
                 PipelineName = "MyPipeline",
                 Synth = new ShellStep("Synth", new ShellStepProps
                 {
-                    Input = CodePipelineSource.GitHub("paul-olofsson/MyTestRepo", "master"),
+                    Input = CodePipelineSource.Connection("MyTestRepo", "master", new ConnectionSourceOptions
+                    {
+                        ConnectionArn = "arn:aws:codestar-connections:eu-west-1:220659591193:connection/45669400-261f-46e2-8316-e7d38470deb2",
+                        TriggerOnPush = true
+                    }),
+                    //Input = CodePipelineSource.GitHub("paul-olofsson/MyTestRepo", "master"),
                     Commands = new string[] { "npm install -g aws-cdk", "cdk synth" }
                 })
             });
