62ba242fa19ff1bdd463b6433799fd9e8b5ca9d1
Paul Olofsson <paul.olofsson@visma.com>
2023-02-01 13:41:05 +0100

    test

--- src/Cdkproj/CdkprojStack.cs
+++ src/Cdkproj/CdkprojStack.cs
@@ -15,7 +15,7 @@ namespace Cdkproj
                 {
                     Input = CodePipelineSource.Connection("MyTestRepo", "master", new ConnectionSourceOptions
                     {
-                        ConnectionArn = "arn:aws:codestar-connections:eu-west-1:220659591193:connection/45669400-261f-46e2-8316-e7d38470deb2",
+                        ConnectionArn = "arn:aws:codestar-connections:eu-west-1:220659591193:connection/188e1149-5dd5-4854-b70c-313f9cd8dd35",
                         TriggerOnPush = true
                     }),
                     //Input = CodePipelineSource.GitHub("paul-olofsson/MyTestRepo", "master"),
