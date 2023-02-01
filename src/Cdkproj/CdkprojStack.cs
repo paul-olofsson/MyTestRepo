a63853b39b56301f0b219f4271f5047dfdd4a794
Paul Olofsson <paul.olofsson@visma.com>
2023-02-01 09:52:06 +0100

    teststage

--- src/Cdkproj/CdkprojStack.cs
+++ src/Cdkproj/CdkprojStack.cs
@@ -15,13 +15,29 @@ namespace Cdkproj
                 PipelineName = "MyPipeline",
                 Synth = new ShellStep("Synth", new ShellStepProps
                 {
-                    Input = CodePipelineSource.GitHub("VSAUT/filebox",
-                        "net6-upgrade"),
+                    Input = CodePipelineSource.GitHub("paul-olofsson/MyTestRepo",
+                        "master"),
                     Commands = new string[] { "npm install -g aws-cdk", "cdk synth" }
                 })
             });
 
+            pipeline.AddStage(new MyPipelineAppStage(this, "FirstStage", new StageProps
+            {
+                Env = props.Env,
+                StageName = "Nisse"
+            }));
 
+        }
+    }
+
+    class MyPipelineAppStage : Stage
+    {
+        public MyPipelineAppStage(Construct scope, string id, StageProps props=null) : base(scope, id, props)
+        {
+            new Bucket(this, "MyTestBucket", new BucketProps
+            {
+                Versioned = true, RemovalPolicy = RemovalPolicy.DESTROY
+            });
         }
     }
 }
